e6ac4f773a7b70b234ba9d1978d1c70286309da0
Terrabit70 <evgtarak@sibmail.com>
2018-10-02 21:39:22 +0700

    initial commit

---
+++ guess number.py
@@ -0,0 +1,29 @@
+import random
+
+print('******************************')
+print('*         Угадай число       *')
+print('*          от 0 до 20        *')
+print('*         за 6 попыток       *')
+print('******************************')
+
+count = 6
+zag_val = random.randint(0, 20)
+#print('Попыток осталось: ', count)
+#print('Загаданное число ', zag_val)
+while count > 0:
+    print('Попыток осталось: ', count)
+    print('Введите Ваш вариант: ', end='')
+    inp_val = input()
+    if inp_val.isdigit() == False:
+        print('Ты втираешь мне какую-то дичь!')
+    else:
+        if int(inp_val) > zag_val:
+            print('Жадность фраера сгубила!')
+        elif int(inp_val) < zag_val:
+            print('Маловато! Маловато будет!')
+        else:
+            print('Заебись, чотко!')
+            break
+    count -= 1
+else:
+    print('Потрачено!')
